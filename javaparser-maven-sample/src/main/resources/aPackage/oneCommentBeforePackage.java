72ebd3508afd7528844198e635ea7e91eb24e246
MathieuDesponds <mathieu.desponds@epfl.ch>
2020-02-22 16:50:25 +0100

    Change the structure of the branch
    Remove one in-between folder that was unconvenient

--- javaparser-maven-sample/src/main/resources/aPackage/oneCommentBeforePackage.java
+++
@@ -1,9 +0,0 @@
-//comment 1
-package aPackage;
-public class commentedClass {
-    public boolean foo(int i, int j) {
-        if (i == 1 || j == 0)
-            return true;
-        return false;
-    }
-}
